610e29d693d84db749ba0af7bc42b1f4f3a8276e
Ben Mitchell <benmitchelliii@gmail.com>
2024-10-04 23:45:42 -0400

    adding airflow structure

---
+++ airflow/src/main/java/com/ten31f/autogatalog/Action.java
@@ -0,0 +1,24 @@
+package com.ten31f.autogatalog;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Action {
+
+	SCAN("scan"), DOWNLOAD("download"), IMAGE("image"), HEALTH("health"), JUNK("");
+
+	private String cliString;
+
+	public static Action findByString(String value) {
+
+		for (Action action : values()) {
+			if (action.getCliString().equals(value))
+				return action;
+		}
+
+		return JUNK;
+	}
+
+}
---
+++ airflow/src/main/java/com/ten31f/autogatalog/DAG.java
@@ -0,0 +1,68 @@
+package com.ten31f.autogatalog;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.ten31f.autogatalog.tasks.Downloadrequestor;
+import com.ten31f.autogatalog.tasks.HealthCheck;
+import com.ten31f.autogatalog.tasks.ImageGrabber;
+import com.ten31f.autogatalog.tasks.Scan;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@AllArgsConstructor
+@Slf4j
+@Getter
+@SpringBootApplication
+public class DAG implements CommandLineRunner {
+
+	public Scan scan;
+	public Downloadrequestor downloadrequestor;
+	public ImageGrabber imageGrabber;
+	public HealthCheck healthCheck;
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DAG.class, args).close();
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		log.info(String.format("args: %s", String.join(",", Arrays.asList(args))));
+
+		if (args.length != 1) {
+			log.error("operation required");
+			return;
+		}
+
+		switch (Action.findByString(args[0])) {
+		case DOWNLOAD:
+			getDownloadrequestor().run();
+			break;
+		case HEALTH:
+			getHealthCheck().run();
+			break;
+		case IMAGE:
+			getImageGrabber().run();
+			break;
+		case SCAN:
+			getScan().run();
+			break;
+		default:
+			log.error(String.format("\"%s\" does not match any know operation %s", args[0],
+					String.join(",", Stream.of(Action.values()).map(Action::getCliString).toList())));
+			break;
+
+		}
+
+	}
+
+}
